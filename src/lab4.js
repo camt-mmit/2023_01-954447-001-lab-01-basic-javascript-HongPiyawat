f37751faa90fa392379a015adb235c4ff1b9107b
Piyawat <hongkung4826@gmail.com>
2023-06-19 18:28:39 +0700

    Lab1

--- src/lab4.js
+++ src/lab4.js
@@ -2,17 +2,20 @@ import { createInterface } from "node:readline";
 
 const rl = createInterface(process.stdin,process.stdout);
 
-rl.question('Input size: ', (size) => {
-        const n = size;
-        for (let j = 1; j <= n; j++) {
-            let line = '';
-            for (let i = 1; i <= n - j; i++) {
-                line += ' '.padStart(5,' ');
-            }
-            for (let k = 1; k <= j; k++) {
-                line += '*'.padStart(5,' ');
-            }
-            console.info(line)
+rl.question('Input size: ', (nText) => {
+        const n = parseInt(nText);
+
+    for(let j = 0; j < n; j++) {
+        let line = '';
+        for (let i = 0; i < n; i++) {
+            line +=
+            (
+                i === 0 ||
+                j === 0 ||
+                i === n - 1 ||
+                j === n - 1
+            )? '*' : ' ' 
         }
-    rl.close();
+        console.info(line);
+    }
 });
